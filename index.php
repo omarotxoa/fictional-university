b3737fb9acd6ab93d0b40106a925bce37f4f4260
Omar Ochoa <omar@ochoawebdesign.com>
2022-08-26 13:48:33 -0700

    Create and include footer.php and header.php

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php get_header(); ?>
+
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
     <h2>
@@ -6,8 +8,10 @@
       </a>
     </h2>
 
-    <?php the_content(); ?>
+    <?php the_excerpt(); ?>
 
     <hr>
 
 <?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
--- page.php
+++ page.php
@@ -1,6 +1,11 @@
+<?php get_header(); ?>
+
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
     <p>This is a page not a post</p>
     <h2><?php the_title(); ?></h2>
-
     <?php the_content(); ?>
+    
 <?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
