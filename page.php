f5253b67cd37c5a94a9a2aaba40c1dabb98627cb
Omar Ochoa <omar@ochoawebdesign.com>
2022-08-26 13:41:39 -0700

    Create page.php and single.php

--- page.php
+++ page.php
@@ -1,13 +1,6 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-    <h2>
-      <a href="<?php the_permalink(); ?>">
-        <?php the_title(); ?>
-      </a>
-    </h2>
+    <p>This is a page not a post</p>
+    <h2><?php the_title(); ?></h2>
 
     <?php the_content(); ?>
-
-    <hr>
-
 <?php endwhile; endif; ?>
