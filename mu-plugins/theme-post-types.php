e87abd34ea62b282b2ee309cda0b43c2047c8aa6
Omar Ochoa <omar@ochoawebdesign.com>
2022-09-02 15:25:28 -0700

    Created MU Plugin theme-post-types.php, scripted Events post type

---
+++ mu-plugins/theme-post-types.php
@@ -0,0 +1,16 @@
+<?php
+function theme_post_types() {
+  register_post_type('event', array(
+    'public' => true,
+    'labels' => array(
+      'name' => 'Events',
+      'add_new_item' => 'Add New Event',
+      'edit_item' => 'Edit Event',
+      'all_items' => 'All Events',
+      'singular_name' => 'Event'
+    ),
+    'menu_icon' => 'dashicons-calendar-alt'
+  ));
+}
+add_action('init', 'theme_post_types');
+?>
