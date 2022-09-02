9e192014612e915346faa14d448398935f8dd610
Omar Ochoa <omar@ochoawebdesign.com>
2022-09-02 15:50:14 -0700

    Refactor MU Plugin Theme Post TYpes: Slug rewrite from 'event' to 'events'

--- mu-plugins/theme-post-types.php
+++ mu-plugins/theme-post-types.php
@@ -1,6 +1,7 @@
 <?php
 function theme_post_types() {
   register_post_type('event', array(
+    'rewrite' => array('slug' => 'events'),
     'public' => true,
     'show_in_rest' => true,
     'labels' => array(
