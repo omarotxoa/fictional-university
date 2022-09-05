cd02edf566188bee7a89bfcf4a8cadbce29a95d5
Omar Ochoa <omar@ochoawebdesign.com>
2022-09-05 14:22:50 -0700

    Script WP_Query manipulation for Events Archive

--- themes/fictional-university-theme/functions.php
+++ themes/fictional-university-theme/functions.php
@@ -17,4 +17,27 @@
     add_theme_support('title-tag');
   }
   add_action('after_setup_theme', 'university_features'); 
+
+  /* This function makes adjustments to the default WP_Query. */
+  function wp_query_adjustments($query) {
+    /* 
+      This if check makes sure that the code only applies to the MAIN QUERY on the FRONTEND in the EVENT custom post type.
+      The main WP_Query applies to the WHOLE website, even the admin section. So it is important to apply appropriate restrictions. 
+    */
+    $today = date('Ymd');
+    if($query->is_main_query() && !is_admin() && is_post_type_archive('event')) {
+      $query->set('meta_key', 'event_date');
+      $query->set('orderby', 'meta_value');
+      $query->set('order', 'ASC');
+      $query->set('meta_query', array(
+        array(
+          'key' => 'event_date',
+          'compare' => '>=',
+          'value' => $today,
+          'type' => 'numeric'
+        )
+      ));
+    }
+  }
+  add_action('pre_get_posts', 'wp_query_adjustments');
 ?>
