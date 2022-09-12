55b2e3b3e26ed6d03863ee48bd6acbd2f330a1fa
Omar Ochoa <omar@ochoawebdesign.com>
2022-09-12 15:14:15 -0700

    Create single-professor.php

---
+++ themes/fictional-university-theme/single-professor.php
@@ -0,0 +1,34 @@
+<?php get_header(); ?>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>"></div>
+        <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php the_title(); ?></h1>
+        <div class="page-banner__intro">
+            <p>Join us for a few hours of fun</p>
+        </div>
+    </div>
+</div>
+
+<div class="container container--narrow page-section">
+    <div class="generic-content"><?php the_content(); ?></div>
+
+    <?php 
+        $related_programs = get_field('related_programs'); 
+        if($related_programs) { 
+    ?>
+        <hr class="section-break">
+        <h2 class="headline headline--medium">Subject(s) Taught</h2>
+        <ul class="link-list min-list">
+            <?php foreach($related_programs as $program) { ?>
+                <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program);?></a></li>
+            <?php } ?>
+        </ul>
+    <?php } ?>
+</div>
+
+<?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
