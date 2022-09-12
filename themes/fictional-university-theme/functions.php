96c189ebca429bfdfe332acffed3dc1092a21e25
Omar Ochoa <omar@ochoawebdesign.com>
2022-09-12 16:21:57 -0700

    Functions.php add page-banner image size

--- themes/fictional-university-theme/functions.php
+++ themes/fictional-university-theme/functions.php
@@ -18,6 +18,7 @@
     add_theme_support('post-thumbnails');
     add_image_size('professor-landscape', 400, 260, true);
     add_image_size('professor-portrait', 480, 650, true);
+    add_image_size('page-banner', 1500, 350, true);
   }
   add_action('after_setup_theme', 'university_features'); 
 
