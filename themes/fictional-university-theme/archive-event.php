74513c16ec9c98d7d3af1ed9e887c85f939172eb
Omar Ochoa <omar@ochoawebdesign.com>
2022-09-14 21:01:12 -0700

    Add page_banner() without arguments to select single and archive pages

--- themes/fictional-university-theme/archive-event.php
+++ themes/fictional-university-theme/archive-event.php
@@ -1,14 +1,6 @@
 <?php get_header(); ?>
 
-<div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>"></div>
-        <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">All Events</h1>
-        <div class="page-banner__intro">
-            <p>All the events going on.</p>
-        </div>
-    </div>
-</div>
+<?php page_banner(); ?>
 
 <div class="container container--narrow page-section">
   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
