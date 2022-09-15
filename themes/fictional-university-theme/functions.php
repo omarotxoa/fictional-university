c5bab876d5b7af64be560b67afe0f34687069dc5
Omar Ochoa <omar@ochoawebdesign.com>
2022-09-14 20:59:05 -0700

    Refactor banner_page() in functions.php

--- themes/fictional-university-theme/functions.php
+++ themes/fictional-university-theme/functions.php
@@ -1,10 +1,32 @@
-<?php function page_banner() { ?>    
+<?php 
+  function page_banner($args = NULL) { 
+    if (!$args['title']) {
+      $args['title'] = get_the_title(); 
+    }
+
+    if (!$args['subtitle']) {
+      $args['subtitle'] = get_field('page_banner_subtitle'); 
+    }
+
+    if (!$args['photo']) {
+      if (get_field('page_banner_background_image') AND !is_archive() AND !is_home()) {
+        $args['photo'] = get_field('page_banner_background_image')['sizes']['page-banner'];
+      } else {
+        $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
+      }
+    }
+    
+?>    
   <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php $page_banner_image = get_field('page_banner_background_image'); echo $page_banner_image['sizes']['page-banner'] ?>"></div>
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']; ?>"></div>
       <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title"><?php the_title(); ?></h1>
+      <h1 class="page-banner__title">
+        <?php 
+          echo $args['title'];
+        ?>
+      </h1>
       <div class="page-banner__intro">
-          <p><?php the_field('page_banner_subtitle'); ?></p>
+          <p><?php echo $args['subtitle']; ?></p>
       </div>
     </div>
   </div>
