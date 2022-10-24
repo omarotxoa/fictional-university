c3109b525285aa69371f8574b143732c28cf4c81
Omar Ochoa <omar@ochoawebdesign.com>
2022-10-24 16:34:55 -0700

    Feature CUSTOM REST API: Funnels data to their appropriate array

--- themes/fictional-university-theme/inc/search-route.php
+++ themes/fictional-university-theme/inc/search-route.php
@@ -11,18 +11,56 @@
 
   function universitySearchResults($data) {
     $mainQuery = new WP_Query(array(
-      'post_type' => array('post', 'page', 'professor'),
+      'post_type' => array('post', 'page', 'professor', 'program', 'campus', 'event'),
       's' => $data['term']
     )); 
 
-    $results = array();
+    $results = array(
+      'generalInfo' => array(),
+      'professors' => array(),
+      'programs' => array(),
+      'events' => array(),
+      'campuses' => array()
+    );
 
     while ($mainQuery->have_posts()) {
       $mainQuery-> the_post();
-      array_push($results, array(
-        'title' => get_the_title(),
-        'permalink' => get_the_permalink()
-      ));
+
+      if(get_post_type() == 'post' OR get_post_type() == 'page') {
+        array_push($results['generalInfo'], array(
+          'title' => get_the_title(),
+          'permalink' => get_the_permalink()
+        ));
+      }
+
+      if(get_post_type() == 'professor') {
+        array_push($results['professors'], array(
+          'title' => get_the_title(),
+          'permalink' => get_the_permalink()
+        ));
+      }
+
+      if(get_post_type() == 'program') {
+        array_push($results['programs'], array(
+          'title' => get_the_title(),
+          'permalink' => get_the_permalink()
+        ));
+      }
+
+      if(get_post_type() == 'campus') {
+        array_push($results['campuses'], array(
+          'title' => get_the_title(),
+          'permalink' => get_the_permalink()
+        ));
+      }
+
+      if(get_post_type() == 'event') {
+        array_push($results['events'], array(
+          'title' => get_the_title(),
+          'permalink' => get_the_permalink()
+        ));
+      }
+      
     }
 
     return $results;
