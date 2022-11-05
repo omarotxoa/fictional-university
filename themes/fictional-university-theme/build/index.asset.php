73e8eba64e54bac3fca75619b8d3e3a068ec8be7
Omar Ochoa <omar@ochoawebdesign.com>
2022-11-04 18:43:19 -0700

    Feature MyNotes: Non JQuery version

--- themes/fictional-university-theme/build/index.asset.php
+++ themes/fictional-university-theme/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => 'f413f6f3839d1d4477b7');
+<?php return array('dependencies' => array('jquery'), 'version' => 'ca8557bec7e8b6e6cc7e');
