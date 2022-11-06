070ac3d3e4a8e764a3117b4e4a085a52976a62bb
Omar Ochoa <omar@ochoawebdesign.com>
2022-11-05 18:20:39 -0700

    Feature Likebox: Users can now only like a professor once

--- themes/fictional-university-theme/inc/like-route.php
+++ themes/fictional-university-theme/inc/like-route.php
@@ -17,14 +17,31 @@ function universityLikeRoutes() {
 function createLike($data) {
   if(is_user_logged_in()) {
     $professor = sanitize_text_field($data['professorId']);
-    return wp_insert_post(array(
-      'post_type' => 'like',
-      'post_status' => 'publish',
-      'post_title' => 'php like post test 4',
-      'meta_input' => array(
-        'liked_professor_id' => $professor
-      )
+
+    $userHasLikedProfessor = new WP_Query(array(
+        'author' => get_current_user_id(),
+        'post_type' => 'like', 
+        'meta_query' => array(
+            array(
+                'key' => 'liked_professor_id',
+                'compare' => '=',
+                'value' => $professor
+            )
+        )
     ));
+
+    if($userHasLikedProfessor->found_posts == 0 AND get_post_type($professor) == 'professor') {
+      return wp_insert_post(array(
+        'post_type' => 'like',
+        'post_status' => 'publish',
+        'post_title' => 'php like post test 4',
+        'meta_input' => array(
+          'liked_professor_id' => $professor
+        )
+      ));
+    } else {
+      die("Invalid professor id");
+    }
   } else {
     die("Only logged in users can create a like");
   }
