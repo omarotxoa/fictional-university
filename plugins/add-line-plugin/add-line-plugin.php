2b0d1c2fa81a871a92cee42b95ea6beff14964b8
Omar Ochoa <omar@ochoawebdesign.com>
2022-11-07 15:39:39 -0800

    Add Line Plugin: Simple working setup

--- plugins/add-line-plugin/add-line-plugin.php
+++ plugins/add-line-plugin/add-line-plugin.php
@@ -6,4 +6,14 @@
   Author: Omar
   Author URI: https://pluginlink.plugin
 */
+
+add_filter('the_content', 'addToEndOfPost');
+
+function addToEndOfPost($content) {
+  if  (is_single() && is_main_query()) {
+    return $content . '<p>This is a test</p>';
+  }
+
+  return $content;
+}
 ?>
