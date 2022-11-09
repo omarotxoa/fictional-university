9d878fe4562222ae2790d0dc775394df45d8ab99
Omar Ochoa <omar@ochoawebdesign.com>
2022-11-08 18:00:27 -0800

    Word Filter Plugin: Settings page markup and css file

--- plugins/word-filter-plugin/index.php
+++ plugins/word-filter-plugin/index.php
@@ -17,13 +17,27 @@ class WordFilterPlugin {
   }
 
   function wordFilterMenu() {
-    add_menu_page('Words to Filter', 'Word Filter', 'manage_options', 'wordfilter', array($this, 'wfp_settings_page'), plugin_dir_url(__FILE__) . 'custom.svg', 100);
+    $mainPageHook = add_menu_page('Words to Filter', 'Word Filter', 'manage_options', 'wordfilter', array($this, 'wfp_settings_page'), plugin_dir_url(__FILE__) . 'custom.svg', 100);
     add_submenu_page('wordfilter', 'Words to Filter', 'Words List', 'manage_options', 'wordfilter', array($this, 'wfp_settings_page'));
     add_submenu_page('wordfilter', 'Word Filter Options', 'Options', 'manage_options', 'word-filter-options', array($this, 'wfp_options_page'));
+    add_action("load-{$mainPageHook}", array($this, 'mainPageAssets'));
+  }
+
+  function mainPageAssets() {
+    wp_enqueue_style('filterAdminCss', plugin_dir_url(__FILE__) . 'styles.css');
   }
 
   function wfp_settings_page() { ?>
-    Hello World
+    <div class="wrap">
+      <h1>Word Filter Settings</h1>
+      <form method="POST">
+        <label for="plugin_words_to_filter"><p>Enter a <strong>comma-separated</strong> list of words to filter from your site's content.</p></label>
+        <div class="word-filter__flex-container">
+          <textarea name="plugin_words_to_filter" id="plugin_words_to_filter" placeholder="bad, mean, awful, horrible, words"></textarea>
+        </div>
+        <input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes">
+      </form>
+    </div>
   <?php }
 
   function wfp_options_page() { ?>
