afb737e8400bbd3ad9ad67cb906474d47effd7f3
Omar Ochoa <omar@ochoawebdesign.com>
2022-11-08 17:27:15 -0800

    Word Filter Plugin: basic menu pages

--- plugins/word-filter-plugin/index.php
+++ plugins/word-filter-plugin/index.php
@@ -13,8 +13,22 @@ if( !defined('ABSPATH')) exit;
 
 class WordFilterPlugin {
   function __construct() {
-    
+    add_action('admin_menu', array($this, 'wordFilterMenu'));
   }
+
+  function wordFilterMenu() {
+    add_menu_page('Words to Filter', 'Word Filter', 'manage_options', 'wordfilter', array($this, 'wordFilterPage'), 'dashicons-smiley', 100);
+    add_submenu_page('wordfilter', 'Word Filter Options', 'Options', 'manage_options', 'word-filter-options', array($this, 'optionsSubPage'));
+  }
+
+  function wordFilterPage() { ?>
+    Hello World
+  <?php }
+
+  function optionsSubPage() { ?>
+    Hello World from the options page
+  <?php }
+
 }
 
 $wordFilterPlugin = new WordFilterPlugin();
