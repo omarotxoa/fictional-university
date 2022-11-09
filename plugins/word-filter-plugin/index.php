ecc762932cf64857a4de34c090fd1ff986eb77fb
Omar Ochoa <omar@ochoawebdesign.com>
2022-11-08 17:19:31 -0800

    Word Filter Plugin: Create base

---
+++ plugins/word-filter-plugin/index.php
@@ -0,0 +1,20 @@
+<?php 
+/*
+  Plugin Name: Word Filter Plugin
+  Description: An awesome custom plugin. 
+  Version: 1.0
+  Author: Omar
+  Author URI: https://pluginlink.plugin
+*/
+
+// A security measure
+// exit if accessed directly
+if( !defined('ABSPATH')) exit; 
+
+class WordFilterPlugin {
+  function __construct() {
+    
+  }
+}
+
+$wordFilterPlugin = new WordFilterPlugin();
