5f9d65a580c09349d509846efabc3f72ec10327c
Omar Ochoa <omar@ochoawebdesign.com>
2022-11-18 20:44:01 -0800

    delegating the frontend output to the index.php file

--- plugins/a-javascript-plugin/index.php
+++ plugins/a-javascript-plugin/index.php
@@ -11,11 +11,21 @@ if( !defined('ABSPATH')) exit;
 
 class AJavascriptPlugin {
   function __construct() {
-    add_action('enqueue_block_editor_assets', array($this, 'admin_assets'));
+    add_action('init', array($this, 'admin_assets'));
   }
 
   function admin_assets() {
-    wp_enqueue_script('our_new_block_type', plugin_dir_url(__FILE__) . '/build/index.js', array('wp-blocks', 'wp-element'));
+    wp_register_script('our_new_block_type', plugin_dir_url(__FILE__) . '/build/index.js', array('wp-blocks', 'wp-element'));
+    register_block_type('ajp/a-javascript-plugin', array(
+      'editor_script' => 'our_new_block_type',
+      'render_callback' => array($this, 'theHTML')
+    ));
+  }
+
+  function theHTML($attributes) {
+    ob_start(); ?>
+    <h1>Today the sky is completely <?php echo $attributes['skyColor']; ?> and the grass is <?php echo $attributes['grassColor']; ?> !!!</h1>
+    <?php return ob_get_clean();
   }
 }
 
